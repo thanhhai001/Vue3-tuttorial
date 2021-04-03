85e0e586c56ce53d385cf620515ebdb3fc7d34de
thanhhai001 <haicnt.iuh@gmail.com>
2021-04-03 14:16:59 +0700

    first commit
    first commit
    
    Co-Authored-By: thanhhai001 <42108720+Thanhhai001@users.noreply.github.com>

---
+++ twitter-tutorial/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="app">
+  <UserProfile/>
+</div>
+<!-- <div id='app'>
+  @_{{user.userName}} - {{fullName}}
+  <strong>Followers:</strong> {{user.followers}}
+</div> -->
+<!-- <button @click="followUser()">Follow</button> -->
+</template>
+
+<script>
+import UserProfile from './components/Layout/UserProfile.vue';
+export default {
+  name: 'App',
+  components: {
+    UserProfile
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ twitter-tutorial/src/components/Layout/UserProfile.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile-content">
+      <h1>@_{{user.userName}}</h1>
+      <div class="user-profile-role">
+        {{user.roleName}}
+      </div>
+      <div class="user-profile-follower">
+        Followers: {{user.followers}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'UserProfile',
+    data() {
+      return {
+          user: {
+            id : 1,
+            userName: 'dark01',
+            email: 'haicnt.iuh@gmail.com',
+            firstName: 'Cao Ngoc Thanh',
+            lastName: 'Hai',
+            isAdmin: true,
+            followers: 0,
+            role: 1,
+            roleName: 'Admin'
+          }
+      }
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    }
+  },
+  methods: {
+    followUser() {
+      this.user.followers++;
+    }
+  }
+}
+</script>
+
+<style>
+  .user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    padding: 50px 5%;
+  }
+
+  .user-profile-content {
+    display: flex;
+    flex-direction: column;
+    margin-right: 50px;
+    background: white;
+    padding: 20px;
+    border-radius: 5px;
+    border: 1px solid #DFE3E8;
+    text-align: left;
+    
+  }
+
+  .user-profile-follower {
+
+  }
+</style>
